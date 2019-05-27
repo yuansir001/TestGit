c09b95cee37e22fc32d43cb2708cc2dec8113f16
eclipse_user <eclipse_user@qq.com>
2019-05-27 20:01:32 +0800

    edit on branch hot_fix

--- src/main/java/com/atguigu/git/Happy.java
+++ src/main/java/com/atguigu/git/Happy.java
@@ -5,6 +5,7 @@ public class Happy {
 	public static void main(String[] args) {
 		System.out.println("left11....");
 		System.out.println("right....");
+		System.out.println("hot_fix......");
 	}
 	
 }
